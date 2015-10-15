5e8df0663bf199565be0fa6f8d8ce83f0bed38bf
Ryan Yep <ryan.yep@gmail.com>
2015-10-15 14:55:58 -0500

    Add LineitemsController & route

---
+++ app/controllers/lineitems_controller.rb
@@ -0,0 +1,64 @@
+class LineitemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @lineitems = Lineitem.all
+  end
+
+  def show
+  end
+
+  def new
+    @lineitem = Lineitem.new
+  end
+
+  def edit
+  end
+
+  def create
+    @lineitem = Lineitem.new(item_params)
+
+    respond_to do |format|
+      if @lineitem.save
+        format.html { redirect_to @lineitem, notice: 'Lineitem was successfully created.' }
+        format.json { render :show, status: :created, location: @lineitem }
+      else
+        format.html { render :new }
+        format.json { render json: @lineitem.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /items/1
+  # PATCH/PUT /items/1.json
+  def update
+    respond_to do |format|
+      if @lineitem.update(item_params)
+        format.html { redirect_to @lineitem, notice: 'Item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @lineitem }
+      else
+        format.html { render :edit }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @lineitem.destroy
+    respond_to do |format|
+      format.html { redirect_to items_url, notice: 'Lineitem was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_lineitem
+      @lineitem = Lineitem.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def lineitem_params
+      params.require(:item).permit(:name, :description, :price, :quantity)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :orders
   resources :items
   resources :users
+  resources :lineitems
 
   root 'items#index'
 end
