dfe907d78af913bca2d1c638d52c046bcca6b53e
Ryan Yep <ryan.yep@gmail.com>
2015-10-16 15:39:13 -0500

    Fixed issue with start new order button not showing up after an order had been completed/paid equaling true and added styling to order show page.

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,8 @@ class Order < ActiveRecord::Base
   def self.user_order_exists?(user)
     where(user_id: user.id).empty? if user
   end
+
+  def self.user_has_unpaid_order?(user)
+    where(user_id: user.id, paid: false)
+  end
 end
