b6326be33fd1493e1712b0dc7f6536abcdba44c8
Ryan Yep <ryan.yep@gmail.com>
2015-10-16 17:32:48 -0500

    Changed .empty? to .any? to check if any order exists for current user

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,7 @@ class Order < ActiveRecord::Base
   has_many :items, through: :lineitems
 
   def self.user_order_exists?(user) #checks to see if there is an empty array or if there is an order opened already by current user
-    where(user_id: user.id).empty? || where(user_id: user.id) if user
+    where(user_id: user.id).any? if user
   end
 
   def self.user_has_unpaid_order?(user) #checks to see if there is an incomplete order (unpaid)
