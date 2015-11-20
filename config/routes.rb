23b04da706078fad9b788f3d6ff60951c0480a0c
Ryan Yep <ryan.yep@gmail.com>
2015-11-19 21:48:31 -0600

    Add missing sessions controller, new sessions view and routes get post and delete

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resources :users
   resources :lineitems
 
+  get     'login',   to: "sessions#new",     as: "new_login"
+  post    '/login',  to: 'sessions#create',  as: 'login'
+  delete  '/logout', to: 'sessions#destroy', as: 'logout'
+
   root 'items#index'
 end
